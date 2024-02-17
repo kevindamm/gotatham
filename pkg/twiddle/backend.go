80cba90a283379dfd9bd65457bb31f644ba7a554
Kevin Damm <github@kevindamm.com>
2024-02-17 12:57:53 -0500

    TwiddleMove datatype

--- pkg/twiddle/backend.go
+++ pkg/twiddle/backend.go
@@ -14,14 +14,36 @@ type twiddleState struct {
   Autosolve bool
   MoveCount int
   MoveTarget int
-  PreviousMove TwiddleMove
-}
-
-type TwiddleMove struct {
-  VSelect int
-  HSelect int
-  Size int
-  Direction int
+  PlayerDoes TwiddleMove
 }
 
 type TwiddleGrid []int
+
+type TwiddleMove int
+
+func NewTwiddleMove(x, y, size int, clockwise bool) {
+  var tm int
+  if clockwise {
+    tm + 1
+  }
+  tm |= x & MASK_DIM << X_OFFSET
+  tm |= y & MASK_DIM << Y_OFFSET
+  tm |= size & MASK_SIZE << SIZE_OFFSET
+  return TwiddleMove(tm)
+}
+
+func (t TwiddleMove) SelectX() int {
+  return int(t)>>X_OFFSET & MASK_DIM
+}
+
+func (t TwiddleMove) SelectY() int {
+  return int(t)>>Y_OFFSET & MASK_DIM
+}
+
+func (t TwiddleMove) Size() int {
+  return int(t)>>SIZE_OFFSET & MASK_SIZE
+}
+
+func (t TwiddleMove) Direction() bool {
+  return int(t) & 1 > 0
+}
